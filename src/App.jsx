8f48a6f0f767388767964454884c82ff0b4a7d06
zura tetrashvili <zuratetra@gmail.com>
2024-11-26 22:37:46 +0400

    create content

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,66 @@ function App() {
   return (
     <>
       <div className="">
-        <img className="w-[700px] h-[500px]" src="/work.jpg" alt="work-image" />
+        <img
+          className="max-w-[700px] h-[500px] m-auto"
+          src="/work.jpg"
+          alt="work-image"
+        />
+        <div className="max-w-[700px]">
+          <h1>FAQ</h1>
+          <h2 className="text-2xl mt-5">How many team members can I invite?</h2>
+          <div className="flex items-center">
+            <p className="mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+              nostrum laboriosam sequi facilis iste similique incidunt laborum
+              amet natus enim. Nisi velit deserunt doloribus ratione! Distinctio
+              rerum id nesciunt illo!
+            </p>
+            <img className="w-6 h-6" src="/arrow.jpg" alt="arrow" />
+          </div>
+          <h2 className="text-2xl mt-5">
+            What is the maximum file upload size?
+          </h2>
+          <div className="flex items-center">
+            <p className="mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+              nostrum laboriosam sequi facilis iste similique incidunt laborum
+              amet natus enim. Nisi velit deserunt doloribus ratione! Distinctio
+              rerum id nesciunt illo!
+            </p>
+            <img className="w-6 h-6" src="/arrow.jpg" alt="arrow" />
+          </div>
+          <h2 className="text-2xl mt-5">How do I reset my password?</h2>
+          <div className="flex items-center">
+            <p className="mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+              nostrum laboriosam sequi facilis iste similique incidunt laborum
+              amet natus enim. Nisi velit deserunt doloribus ratione! Distinctio
+              rerum id nesciunt illo!
+            </p>
+            <img className="w-6 h-6" src="/arrow.jpg" alt="arrow" />
+          </div>
+          <h2 className="text-2xl mt-5">Can i cancel my subscription?</h2>
+          <div className="flex items-center">
+            <p className="mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+              nostrum laboriosam sequi facilis iste similique incidunt laborum
+              amet natus enim. Nisi velit deserunt doloribus ratione! Distinctio
+              rerum id nesciunt illo!
+            </p>
+            <img className="w-6 h-6" src="/arrow.jpg" alt="arrow" />
+          </div>
+          <h2 className="text-2xl mt-5">Do you provide additional Support?</h2>
+          <div className="flex items-center">
+            <p className="mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+              nostrum laboriosam sequi facilis iste similique incidunt laborum
+              amet natus enim. Nisi velit deserunt doloribus ratione! Distinctio
+              rerum id nesciunt illo!
+            </p>
+            <img className="w-6 h-6" src="/arrow.jpg" alt="arrow" />
+          </div>
+        </div>
       </div>
     </>
   );
